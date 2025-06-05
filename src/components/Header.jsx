a0a6e210eee79ac1629104f31ca36eba1a8f25e5
Fadi ZAATARI <fadizaatari@hotmail.com>
2025-06-05 10:17:57 +0300

    20250605

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
-import { FaRegNewspaper } from "react-icons/fa";
-import { FcElectricity } from "react-icons/fc";
-import logo from "../assets/logo.png";
-import { IoMdLogOut } from "react-icons/io";
-import { AiFillDashboard } from "react-icons/ai";
-import { CgProfile } from "react-icons/cg";
-import { Separator } from "@/components/ui/separator";
 import { FaBars } from "react-icons/fa6";
-import { FaSun, FaMoon } from "react-icons/fa";
-import { FaDatabase } from "react-icons/fa6";
+import { FaSun, FaMoon, FaDatabase, FaRegNewspaper } from "react-icons/fa";
+import { AiFillDashboard } from "react-icons/ai";
+import { IoMdLogOut } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
 
+import logo from "../assets/logo.png";
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+import { Separator } from "@/components/ui/separator";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
@@ -22,19 +20,15 @@ const useTheme = () => {
   }
   return context;
 };
-// 3. Theme Provider Component
-// This component wraps your application and provides the theme context to its children.
+
 const ThemeProvider = ({ children }) => {
-  // Initialize theme from localStorage or default to 'light'
   const [theme, setTheme] = useState(() => {
     const storedTheme = localStorage.getItem("theme");
     return storedTheme || "light";
   });
 
-  // Effect to update the 'theme' class on the body element
-  // and store the theme in localStorage whenever it changes.
   useEffect(() => {
-    const root = document.documentElement; // Get the root HTML element
+    const root = document.documentElement;
     if (theme === "dark") {
       root.classList.add("dark");
       localStorage.setItem("theme", "dark");
@@ -44,7 +38,6 @@ const ThemeProvider = ({ children }) => {
     }
   }, [theme]);
 
-  // Function to toggle the theme
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
@@ -56,12 +49,16 @@ const ThemeProvider = ({ children }) => {
   );
 };
 
-const ThemedIcon = () => {
-  const { theme } = useTheme();
+const ThemeContent = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="flex flex-col align-middle justify-center items-center p-2 cursor-pointer w-full text-2xl">
-      {/* Conditionally render the icon based on the theme */}
-      {theme === "light" ? <FaMoon /> : <FaSun />}
+    <div onClick={toggleTheme}>
+      <div
+        className="flex rounded-full hover:bg-MyGray flex-col align-middle justify-center
+       items-center p-2 cursor-pointer h-full w-full text-2xl focus:outline-none"
+      >
+        {theme === "light" ? <FaMoon size={10} /> : <FaSun size={10} />}
+      </div>
     </div>
   );
 };
@@ -93,89 +90,78 @@ const Header = () => {
         Electricity Generator Consumption Billing
       </p>
       {/* Right side - Dropdown */}
-      <div className="relative">
-        <button
-          className="cursor-pointer bg-gray-800 text-white rounded-full p-2 hover:bg-white hover:text-gray-800 focus:outline-none position-"
-          onClick={toggleDropdown}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          {isHovered ? <FaBars /> : <FaBars />}
-        </button>
+      <div className="grid grid-cols-2 gap-3">
+        <ThemeProvider>
+          <ThemeContent />
+        </ThemeProvider>
 
-        {isOpen && (
-          <div className="absolute right-0 mt-2 w-38 bg-gray-800 rounded shadow-lg z-10">
-            <Link
-              to="/dashboard"
-              className="block rounded py-2 px-4 text-white hover:bg-white hover:text-gray-800"
-            >
-              <div className="flex items-center ">
-                <AiFillDashboard />
-                <p className="pl-2">Dashboard</p>
-              </div>
-            </Link>
-            <Link
-              to="/profile"
-              className="block rounded py-2 px-4 text-white hover:bg-white hover:text-gray-800"
-            >
-              <div className="flex items-center ">
-                <CgProfile />
-                <p className="pl-2">Profile</p>
-              </div>
-            </Link>
+        <div className="relative">
+          <button
+            className="cursor-pointer bg-gray-800 text-white rounded-full p-2 hover:bg-white hover:text-gray-800 focus:outline-none"
+            onClick={toggleDropdown}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            {isHovered ? <FaBars /> : <FaBars />}
+          </button>
 
-            <Link
-              to="/subscriptions"
-              className="block rounded py-2 px-4 text-white hover:bg-white hover:text-gray-800"
-            >
-              <div className="flex items-center">
-                <FaDatabase />
-                <p className="pl-2">My Subscriptions</p>
-              </div>
-            </Link>
-            <Link
-              to="/news"
-              className="block rounded py-2 px-4 text-white hover:bg-white hover:text-gray-800"
-            >
-              <div className="flex items-center ">
-                <FaRegNewspaper />
-                <p className="pl-2">News</p>
-              </div>
-            </Link>
+          {isOpen && (
+            <div className="absolute right-0 mt-2 w-38 bg-gray-800 rounded shadow-lg z-10">
+              <Link
+                to="/dashboard"
+                className="block rounded py-2 px-4 text-white hover:bg-white hover:text-gray-800"
+              >
+                <div className="flex items-center ">
+                  <AiFillDashboard />
+                  <p className="pl-2">Dashboard</p>
+                </div>
+              </Link>
+              <Link
+                to="/profile"
+                className="block rounded py-2 px-4 text-white hover:bg-white hover:text-gray-800"
+              >
+                <div className="flex items-center ">
+                  <CgProfile />
+                  <p className="pl-2">Profile</p>
+                </div>
+              </Link>
 
-            <Separator className="mt-1 mb-1 bg-white dark:bg-gray-500" />
+              <Link
+                to="/subscriptions"
+                className="block rounded py-2 px-4 text-white hover:bg-white hover:text-gray-800"
+              >
+                <div className="flex items-center">
+                  <FaDatabase />
+                  <p className="pl-2">My Subscriptions</p>
+                </div>
+              </Link>
+              <Link
+                to="/news"
+                className="block rounded py-2 px-4 text-white hover:bg-white hover:text-gray-800"
+              >
+                <div className="flex items-center ">
+                  <FaRegNewspaper />
+                  <p className="pl-2">News</p>
+                </div>
+              </Link>
 
-            <button
-              className="block rounded py-2 px-4 w-full text-white hover:bg-white hover:text-gray-800 text-left cursor-pointer"
-              onClick={handleSignOut}
-            >
-              <div className="flex items-center ">
-                <IoMdLogOut />
+              <Separator className="mt-1 mb-1 bg-white dark:bg-gray-500" />
 
-                <p className="pl-2">Logout</p>
-              </div>
-            </button>
-            <Separator className="mt-1 mb-1 bg-white dark:bg-gray-500" />
-            <div className="bg-gray-800 flex flex-col">
-              <ThemeProvider>
-                <ThemeContent />
-              </ThemeProvider>
+              <button
+                className="block rounded py-2 px-4 w-full text-white hover:bg-white hover:text-gray-800 text-left cursor-pointer"
+                onClick={handleSignOut}
+              >
+                <div className="flex items-center ">
+                  <IoMdLogOut />
+                  <p className="pl-2">Logout</p>
+                </div>
+              </button>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
 };
 
 export default Header;
-
-// Separate component to consume theme context within ThemeProvider
-const ThemeContent = () => {
-  const { theme, toggleTheme } = useTheme();
-  return (
-    <div onClick={toggleTheme}>
-      <ThemedIcon />
-    </div>
-  );
-};
