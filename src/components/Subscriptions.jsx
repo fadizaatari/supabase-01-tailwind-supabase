ce12fdc100df392d0a141bc585e90b01f9c80ac4
Fadi ZAATARI <fadizaatari@hotmail.com>
2025-06-06 22:21:28 +0300

    2

--- src/components/Subscriptions.jsx
+++ src/components/Subscriptions.jsx
@@ -1,3 +1,15 @@
+import { FaCircleInfo } from "react-icons/fa6";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
 import { Checkbox } from "@/components/ui/checkbox";
 import { SubscriptionStatus } from "./Myfunctions.js";
 import { Link, useNavigate } from "react-router-dom";
@@ -43,6 +55,10 @@ const Subscriptions = () => {
   const id = session.user.id;
   const fieldid = "userid";
   const { data, loading, error } = useSupabaseData(tableName, fieldid, id);
+
+  const handleButtonClick = (event) => {
+    event.preventDefault(); // Stop the click event from bubbling up to the div
+  };
 
   if (loading) {
     return <LoadingData />;
@@ -110,8 +126,8 @@ const Subscriptions = () => {
                 <div className="xl:bg-blue-950 lg:bg-green-500 md:bg-red-200 sm:bg-blue-200 bg-yellow-250 rounded-md text-white grid-cols-1 sm:flex-row sm-flex-wrap sm:grid-cols-2  md:flex-wrap md:grid-cols-3  flex-wrap xl:grid-cols-2 lg:flex-wrap grid lg:grid-cols-3 gap-2 justify-center p-3">
                   {data.map((item) => (
                     <Link
-                      hidden={getStatus(item.status)}
                       to={"/subscriptions/" + item.seq}
+                      hidden={getStatus(item.status)}
                     >
                       <div
                         className={`rounded text-black w-full justify-center items-center flex flex-col  ${SubscriptionStatus(
@@ -128,10 +144,18 @@ const Subscriptions = () => {
                         <p className="text-sm font-bold text-center mb-2">
                           {item.size}
                         </p>
-
-                        <p className="text-xl font-bold text-center mb-2">
-                          {item.type}
-                        </p>
+                        <div className="flex flex-row w-full">
+                          <div className="w-4/5 flex  align-middle justify-center">
+                            <p className="text-xl font-bold text-center mb-2">
+                              {item.type}
+                            </p>
+                          </div>
+                          <div className="w-1/5 flex min-h-full align-top justify-end items-end">
+                            <button onClick={handleButtonClick}>
+                              <DrawerDemo />
+                            </button>
+                          </div>
+                        </div>
                       </div>
                     </Link>
                   ))}
@@ -148,3 +172,43 @@ const Subscriptions = () => {
 };
 
 export default Subscriptions;
+
+export function DrawerDemo() {
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <button className="text-black">
+          <FaCircleInfo size={25} />
+        </button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Subscription Details</DrawerTitle>
+            <DrawerDescription>
+              You can find all the details here below
+            </DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4 pb-0">
+            <div className="flex items-center justify-center space-x-2">
+              <div className="flex-1 text-center"></div>
+            </div>
+            <div className="mt-3 h-[120px]"></div>
+          </div>
+          <DrawerFooter>
+            <div className="grid grid-cols-2 gap-10">
+              <button className="h-10 dark:bg-white dark:text-black bg-black text-white round-md cursor-pointer">
+                Submit
+              </button>
+              <DrawerClose asChild>
+                <button className="h-10 dark:bg-white dark:text-black bg-black text-white round-md cursor-pointer">
+                  Cancel
+                </button>
+              </DrawerClose>
+            </div>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
